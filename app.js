e1d635ac40583821ea37a951f686abc12e3fb5b9
freemanglenn455-star <freemanglenn455@gmail.com>
2026-09-18 01:18:09 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const PRODUCTS = [
+  {
+    id: "bomber",
+    name: "Signature Rhinestone Bomber",
+    price: 295,
+    priceId: "",
+    image: "assets/bomber.png",
+    tagline: "THE STATEMENT PIECE.",
+    sizes: ["S","M","L","XL","2XL"]
+  },
+  {
+    id: "varsity",
+    name: "Black-on-Black Varsity",
+    price: 225,
+    priceId: "",
+    image: "assets/varsity.png",
+    tagline: "BLACK ON BLACK. NOTHING BASIC ABOUT IT.",
+    sizes: ["S","M","L","XL","2XL"]
+  },
+  {
+    id: "denim",
+    name: "Furry-Patch Denim",
+    price: 175,
+    priceId: "price_1UG1xDFCHNQtBjUv5hlKktab",
+    image: "assets/denim.png",
+    tagline: "DENIM RECONSTRUCTED.",
+    sizes: ["S","M","L","XL","2XL"]
+  },
+  {
+    id: "hoodie",
+    name: "Premium Oversized Hoodie",
+    price: 95,
+    priceId: "price_
